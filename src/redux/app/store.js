467b20bbf55b549301ea6b310aaca15c7ca9a1a5
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-12-19 14:39:05 +0545

    trying to convert into a PWA

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import apireducer from "../features/Reducers/apiReducers"
 import airqualityReducer from '../features/Reducers/airqualityReducer';
 import api from "../features/middleware/api"
@@ -7,9 +7,9 @@ import currentReducer from '../features/Reducers/currentReducer';
 
 export const store = configureStore({
   reducer: {
-    onecallWeather:apireducer,
-    currentWeather:currentReducer,
-    airquality:airqualityReducer,
+    onecallWeather: apireducer,
+    currentWeather: currentReducer,
+    airquality: airqualityReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api),
 });
